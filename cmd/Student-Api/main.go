77fc510d4abb1e88f52a6d8cc23947ff22cd3582
akshay <akshayjha2111@gmail.com>
2025-12-05 14:58:32 +0530

    server setup

--- cmd/Student-Api/main.go
+++ cmd/Student-Api/main.go
@@ -1,13 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
 
+	Config "github.com/akshayjha21/Student-Api/internal/config"
+)
 
-
-func main(){
+func main() {
 	//TODO - load config
 
+	cfg := Config.MustLoad()
 
 	//TODO - database setup
-	//TODO - setup router
+
+	//TODO - setup route
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome to the student api"))
+	})
+
 	//TODO - setup server
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+	fmt.Println("Server has started ")
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("Failed to connect to the server")
+	}
 }
